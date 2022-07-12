7755a8c0b235f9eb87406e48f122376b8180aaa0
ganBaoJun <1936907122@qq.com>
2022-07-12 17:14:50 +0800

    master commit2

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");//蓝色被追踪过，但未修改
         System.out.println("hello git3!");//蓝色文件
         System.out.println("hello git4!");
+        System.out.println("master test");
     }
 }
