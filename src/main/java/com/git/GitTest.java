45e3dfa5e093da8e576b9c1d195306da62e222c4
ganBaoJun <1936907122@qq.com>
2022-07-12 17:00:03 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,5 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");//蓝色被追踪过，但未修改
     }
 }
